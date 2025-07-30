761acda759a6b07dc26d8942d8119eaf03792254
Stanislav Fedotov <st-fedotov@ai-dt.school>
2025-07-30 23:26:29 +0100

    Create __init__.py

---
+++ quiver_representations/__init__.py
@@ -0,0 +1,6 @@
+from .quiver import Quiver
+from .field import FiniteField, ComplexNumbers, ZeroMap
+from .module import Module
+from .morphism import Morphism
+
+__all__ = ['Quiver', 'FiniteField', 'ComplexNumbers', 'Module', 'ZeroMap', 'Morphism']
