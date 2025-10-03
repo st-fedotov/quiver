91bf090c069bb6a64c55bd4e778b8b20b0ce5bc1
Stanislav Fedotov <st-fedotov@ai-dt.school>
2025-10-03 19:31:12 +0100

    Refactor Module and Morphism into separate modules

--- quiver_representations/__init__.py
+++ quiver_representations/__init__.py
@@ -1,5 +1,6 @@
 from .quiver import Quiver
 from .field import Field, FiniteField, ComplexNumbers, ZeroMap
-from .representation import Module, Morphism
+from .module import Module
+from .morphism import Morphism
 
 __all__ = ['Quiver', 'Field', 'FiniteField', 'ComplexNumbers', 'Module', 'ZeroMap', 'Morphism']
