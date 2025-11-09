9a5b9ba1cb7ce59e3fa80117d0ddea27dd235940
Stanislav Fedotov <st-fedotov@ai-dt.school>
2025-11-09 12:46:19 +0100

    Update module_algorithms.py

--- quiver_representations/module_algorithms.py
+++ quiver_representations/module_algorithms.py
@@ -506,17 +506,3 @@ def compute_injective_hull(
 
     return inj_hull, hull_morphism
 
-def get_p_plus_i_dim(Q: quiver):
-    """
-    Returns the sum of all indecomposable projectives and injectives for a given quiver Q
-    """
-    PI = Module.projective(Q, F, 0)[0]
-    for i in range(1, len(Q.vertices)):
-        PI = Module.direct_sum(PI, Module.projective(Q, F, i)[0])[0]
-
-    p_dim = PI.get_dimension_vector()
-
-    for i in range(0, len(Q.vertices)):
-        PI = Module.direct_sum(PI, Module.injective(Q, F, i)[0])[0]
-
-    return p_dim, PI.get_dimension_vector()
